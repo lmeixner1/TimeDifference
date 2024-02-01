7ef964164be1da951b126d6b7be030a15ebe8a23
Lucas Meixner <300083540@fvtc.edu>
2024-02-01 08:20:57 -0600

    Added Gradient and finished application

--- app/src/main/java/edu/fvtc/timediff/MainActivity.java
+++ app/src/main/java/edu/fvtc/timediff/MainActivity.java
@@ -15,54 +15,19 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "MainActivity";
 
+    public EditText etStartTimeInput;
+    public EditText etEndTimeInput;
+    public TextView tvTimeDiffDisplay;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initButton();
-        initTextChangeEvent();
-
+        etStartTimeInput = findViewById(R.id.etStartInput);
+        etEndTimeInput = findViewById(R.id.etEndTimeInput);
     }
 
-    private void initTextChangeEvent() {
-        EditText etStartTimeInput = findViewById(R.id.etStartInput);
-        EditText etEndTimeInput = findViewById(R.id.etEndTimeInput);
-        etStartTimeInput.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                // Log.d(TAG, "beforeTextChanged: ");
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                // Log.d(TAG, "onTextChanged: ");
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                Log.d(TAG, "afterStartTimeChanged: ");
-
-            }
-        });
-
-        etEndTimeInput.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                // Log.d(TAG, "beforeTextChanged: ");
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                // Log.d(TAG, "onTextChanged: ");
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                Log.d(TAG, "afterEndTimeChanged: ");
-
-            }
-        });
-    }
 
     private void initButton() {
         // Get the xml control
@@ -73,16 +38,78 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Log.d(TAG, "onClick: Time Diff clicked");
-                //
-
-
-                // Show the output of the Time Difference
-                TextView etTimeDiffDisplay = findViewById(R.id.tvTimeDiffDisplay);
-
-
-
+                calculateTimeDifference();
 
             }
         });
     }
+
+
+    private void calculateTimeDifference() {
+        //Get controls
+        EditText etStartTimeInput = findViewById(R.id.etStartInput);
+        EditText etEndTimeInput = findViewById(R.id.etEndTimeInput);
+        TextView tvTimeDiffDisplay = findViewById(R.id.tvTimeDiffDisplay);
+
+        //change from text to string
+        String startTime = etStartTimeInput.getText().toString();
+        String endTime = etEndTimeInput.getText().toString();
+
+        //Check the values between the ":"
+        String[] startTimeString = startTime.split(":");
+        String[] endTimeString = endTime.split(":");
+
+        // Check for empty start or end time
+        if (startTime.isEmpty() || endTime.isEmpty()) {
+            tvTimeDiffDisplay.setText(R.string.missing_input_try_again);
+            return;
+        }
+
+        // Check for correct time format
+        if (startTimeString.length != 3 || endTimeString.length != 3) {
+            tvTimeDiffDisplay.setText(R.string.time_formatted_incorrectly_please_use_hh_mm_ss);
+            return;
+        }
+
+        try {
+            // seconds in an hour = 3600, minutes = 60
+            //Convert start and end into seconds since midnight
+            int startSeconds = Integer.parseInt(startTimeString[0]) * 3600 +
+                    Integer.parseInt(startTimeString[1]) * 60 +
+                    Integer.parseInt(startTimeString[2]);
+
+            int endSeconds = Integer.parseInt(endTimeString[0]) * 3600 +
+                    Integer.parseInt(endTimeString[1]) * 60 +
+                    Integer.parseInt(endTimeString[2]);
+
+            //Calculate time difference
+            int timeDifference = 0;
+
+
+            // Calculate time difference in seconds
+            timeDifference = endSeconds - startSeconds;
+
+            if (timeDifference < 0) {
+                tvTimeDiffDisplay.setText(R.string.start_time_is_greater_than_end_time);
+                return;
+            }
+
+            // seconds in an hour = 3600, minutes = 60
+            // convert into hours, and minutes
+            int hours = timeDifference / 3600;
+            int minutes = (timeDifference % 3600) / 60;
+            int seconds = timeDifference % 60;
+
+            //Display result
+            String result = timeDifference + " " + "=" + " " + String.format("%02d:%02d:%02d", hours, minutes, seconds);
+            tvTimeDiffDisplay.setText(result);
+
+        } catch (Exception e) {
+            Log.e(TAG, "Error calculating time difference: Start time is greater than end time");
+            tvTimeDiffDisplay.setText(R.string.start_time_is_greater_than_end_time);
+        }
+
+
+    }
+
 }
